226d0eaa4d326949164abf5ef4892140045e0264
dinnifer silva <dinnifer.silva@aluno.senai.br>
2024-11-27 11:10:54 -0500

    nota e media de um aluno

---
+++ aula27.py
@@ -0,0 +1,8 @@
+nota1 = float(input())
+nota2 = float(input())
+nota3 = float(input())
+nota4 = float(input())
+ 
+media = (nota1 + nota2 + nota3 + nota4) /4
+if media > 8 :
+    print("O aluno foi aprovado")
