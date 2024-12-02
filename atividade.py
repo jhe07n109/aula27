e1e367c0d2dd9d10816616977dbb78cce58230e8
Dinnifer silva <dinnifer.silva@aluno.senai.br>
2024-12-02 09:46:19 -0500

    ver se o aluno passou com a media

---
+++ atividade.py
@@ -0,0 +1,17 @@
+#ler entrada do usuario
+nome= input() #armazenar o nome do aluno
+
+# 4 notas do aluno
+nota1= float(input())
+nota2= float(input())
+nota3= float(input())
+nota4= float(input())
+
+# quantidade de faltas do aluno
+faltas= int(input())
+
+#calculo da media
+media= (nota1 + nota2 + nota3 + nota4)/4
+
+#imprimir na tela
+print (media)
