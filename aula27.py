95167b0b9572b320681b8739d17c9640fd13fc6c
dinnifer silva <dinnifer.silva@aluno.senai.br>
2024-12-05 10:05:32 -0500

    ver qa media do aluno

--- aula27.py
+++ aula27.py
@@ -1,36 +1,59 @@
 
-alunos = []
-cont = 0
-quantidadealunos = int(input("quantidade de alunos: "))
+alunos = [] #alunos
+notas= [] #notas dos alunos
+# menu de situacao de alunos
+while True:
 
-while cont <= quantidadealunos:
-     nome = input("nome ")
-     alunos.append(nome)
-     faltas = int(input("digite a quantidade de faltas "))
+     print ("1.cadastro, 2.ver relatorio, 3.encerrar")
+     decisao= input() #numero de cadastro
+     #cadastro do aluno
+     if decisao =="1":
+          print("faça o cadastro: ")
+          quantidadealuno= int (input("quantidade de aluno: "))
+          cont = 0
+          situacao = ""
+          while cont < quantidadealuno:
+               nome = input("nome do aluno: ")
+               #ler notas e faltas
+               faltas = int(input("quantidades de faltas: "))
+               #4 notas
+               for i in range(4):
+                    notas.append(float(input(f"digite a nota do {i+1} bimestre")))
 
-     print("digite a nota do aluno: ")
-     nota1 = float(input())
-     nota2 = float(input())
-     nota3 = float(input())
-     nota4 = float(input())
-     media = (nota1 + nota2 + nota3 + nota4) /4
+                    #calculo da media
 
+                    media = (notas[0] + notas[1] + notas[2] + notas[3]) /4
+                    #aprovado, recuperacao, reprovado
+               if faltas > 30:
+                    situacao ="O aluno esta reprovado por falta"
+               elif media >= 8 and faltas <= 30: 
+                    situacao = "aprovado"
+               elif media >= 5 and faltas <= 30:
+                    situacao = "recuperação"
+                    recuperacao= float (input("nota da recuperação: "))
+                    calculodarecuperacao= (10-media)
+                    if recuperacao > calculodarecuperacao:
+                         situacao = "aprovado na recuperacao"
+                    else:
+                         situacao = "reprovado na recuperacao"
+               else:
+                    situacao = "reprovado"
 
-     if media >= 8 and faltas <= 30:
-          situacao ="O aluno (A) foi aprovado"
-     elif media >= 5 and faltas <= 30: 
-          situacao = "o aluno esta de recuperação"
-          recuperacao= float (input("nota de recuperação:"))
-          calculodarecuperacao= (10-media)
-          if recuperacao > calculodarecuperacao:
-               situacao = "aprovado na recuperação"
+                    alunos.append({nome, faltas, notas, media, situacao}) #guardando
+                    notas = []
+                    cont += 1
+                    
+     #relatorio
+     elif decisao == "2":
+          if not alunos : #se nao tiver dados do aluno
+               print("ainda nao tem dados de um aluno")
+               #relatorio do aluno
+          for i in alunos:
+               print(f"nome do aluno{i[0]}")
+               print(f"faltas:{i[1]}")
+               print(f"notas:{', '.join(map(str, i[2]))}")
+               print(f"media:{i[3]}")
+               print(f"situacao{i[4]}")
+     #encerramento
           else:
-               situacao = "reprovado na recuperação"
-     else:
-          situacao ="reprovado"
-     cont += 1
-
-     print ("nome:", nome)
-     print("media:", media)
-     print("faltas:", faltas)
-     print("esta: ", situacao)
+               print("the end")
