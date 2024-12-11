c1080fd5492f230878a53bfef278d33b1afb027a
dinnifer silva <dinnifer.silva@aluno.senai.br>
2024-12-11 10:00:42 -0500

    dia tarde noite

--- funcao.py
+++ funcao.py
@@ -1,10 +1,16 @@
 #DECLARAR UMA FUNÇÃO
-def bom_dia():
+def saudacoes (hora_do_dia): #EXIBIR A SAUDAÇÃO CORRESPONDENTE A HORARIOS
+     #DAR BOM DIA
+ 
+   if hora_do_dia >= 0 and hora_do_dia <= 12:
     print("BOM DIA!")
-def boa_tarde():
-    print("BOA TARDE!")
-def boa_noite():
-    print("BOA NOITE!")
-bom_dia()
-boa_tarde()
-boa_noite()
+   elif hora_do_dia >= 13 and hora_do_dia <= 18:
+     print("BOA TARDE!")
+   else:
+     print("BOA NOITE!")
+
+    #FORA DA FUNÇÃO
+resposta = int(input("DIGITE QUE HORAS SÃO: "))
+saudacoes (resposta)
+ 
+  
