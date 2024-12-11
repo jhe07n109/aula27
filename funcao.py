c34ea5b0599c588ae13ef016a3072491ff31a9b8
dinnifer silva <dinnifer.silva@aluno.senai.br>
2024-12-11 08:47:20 -0500

    dia tarde noite

---
+++ funcao.py
@@ -0,0 +1,10 @@
+#DECLARAR UMA FUNÇÃO
+def bom_dia():
+    print("BOM DIA!")
+def boa_tarde():
+    print("BOA TARDE!")
+def boa_noite():
+    print("BOA NOITE!")
+bom_dia()
+boa_tarde()
+boa_noite()
